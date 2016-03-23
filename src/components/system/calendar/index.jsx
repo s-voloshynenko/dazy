f150612546225084197b0fe6362aed412203cb92
s-voloshynenko <s.voloshynenko@gmail.com>
2016-03-23 23:01:58 +0200

    calendar buttons

--- src/components/system/calendar/index.jsx
+++ src/components/system/calendar/index.jsx
@@ -3,13 +3,37 @@ import { calculateMonth } from './utils';
 require('./styles/index.css')
 
 export default class Calendar extends Component {
+  changeMonth(date, e, s) {
+    console.log(date, e, s.eventPhase)
+    // this.setState({
+    //   date: date
+    // })
+  }
+
   renderWeeks() {
     const month = calculateMonth(this.props.date);
 
     return (
       <tbody>
+        <tr>
+          <td colSpan="3">
+            <input className="pull-right calendar-month-btn"
+                   type="button"
+                   value="<"
+                   ref={"2"}
+                   onClick={this.changeMonth} />
+          </td>
+          <td>
+            {month.monthName}
+          </td>
+          <td colSpan="3">
+            <input className="pull-left calendar-month-btn" type="button" value=">" />
+          </td>
+        </tr>
         <tr key={month.monthName}>
-          {month.monthName}
+          {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day, i) => {
+            return <td key={i} className="calendar-day-cell"> {day} </td>;
+          })}
         </tr>
         {month.weeks.map((week, i) => {
           return (
@@ -35,13 +59,6 @@ export default class Calendar extends Component {
   render() {
     return (
       <table className="calendar">
-        <thead>
-          <tr>
-            {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day, i) => {
-              return <td key={i}> {day} </td>;
-            })}
-          </tr>
-        </thead>
         {this.renderWeeks()}
       </table>
     );
