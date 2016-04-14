3d13651a41abf937b42d10400de10a9035c3357b
s-voloshynenko <s.voloshynenko@gmail.com>
2016-04-14 22:57:41 +0300

    window navigation

--- src/components/files/txt/index.jsx
+++ src/components/files/txt/index.jsx
@@ -3,11 +3,19 @@ import FolderIcon from '../../../assets/icons/txt.png';
 require('../styles/index.css');
 
 export default class DefaultFolder extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: this.props.name
+    };
+  }
+
   render() {
     return (
       <figure className="file">
         <p><img src={FolderIcon} alt="" /></p>
-        <figcaption>txt</figcaption>
+        <figcaption>{this.state.name}</figcaption>
       </figure>
     )
   }
--- src/components/window/folder/index.jsx
+++ src/components/window/folder/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import File from '../../files/txt';
 import HideWindowButton from '../../buttons/window/hide';
 import FullScreenButton from '../../buttons/window/fullscreen';
 import CloseWindowButton from '../../buttons/window/close';
@@ -18,18 +19,43 @@ export default class FolderWindow extends Component {
     return (
       <Draggable handle=".window-bar-header" bounds="#desktop">
         <div id="test9" className="window">
-          <div className="window-bar">
-            <div className="window-bar-btns">
-              <HideWindowButton />
-              <FullScreenButton />
-              <CloseWindowButton windowId="test9" />
+          <div className="row">
+            <div className="window-bar">
+              <div className="window-bar-btns">
+                <HideWindowButton />
+                <FullScreenButton />
+                <CloseWindowButton windowId="test9" />
+              </div>
+              <div className="window-bar-header">
+                {this.state.name}
+              </div>
             </div>
-            <div className="window-bar-header">
-              {this.state.name}
+            <div className="window-nav">
+              <ul>
+                <li>
+                  <input type="button" className="window-nav-back" />
+                </li>
+                <li>
+                  <input type="button" className="window-nav-forward" />
+                </li>
+              </ul>
+             </div>
+            <div className="window-content">
+              <div className="pull-left col-md-3 right-divider window-menu">
+                <ul>
+                  <li>
+                    <input type="button" className="window-menu-option" value="Home" />
+                  </li>
+                  <li>
+                    <input type="button" className="window-menu-option" value="Desktop" />
+                  </li>
+                </ul>
+              </div>
+              <div className="pull-right col-md-9">
+                <File name="help.txt" />
+                <File name="Nodejs.txt" />
+              </div>
             </div>
-          </div>
-          <div className="window-nav">
-            {'test'}
           </div>
         </div>
       </Draggable>
